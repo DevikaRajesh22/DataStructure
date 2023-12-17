7c1a3508c9569712aaa859beb6ea09690fc0e2e6
DevikaRajesh22 <devikaraj699@gmail.com>
2023-12-17 21:00:33 +0530

    Heap sort

---
+++ heap/heapSort.js
@@ -0,0 +1,32 @@
+function heapify(arr, length, parentIndex) {
+    let largest = parentIndex;
+    let left = 2 * parentIndex + 1;
+    let right = 2 * parentIndex + 2;
+    if (left < length && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < length && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != parentIndex) {
+        [arr[parentIndex], arr[largest]] = [arr[largest], arr[parentIndex]];
+        heapify(arr, length, largest);
+    }
+    return arr;
+}
+function heapSort(arr) {
+    const length = arr.length;
+    let lastParentIndex = Math.floor(length / 2 - 1);
+    let lastChildIndex = length - 1;
+    while (lastParentIndex >= 0) {
+        heapify(arr, length, lastParentIndex);
+        lastParentIndex--;
+    }
+    while (lastChildIndex >= 0) {
+        [arr[0], arr[lastChildIndex]] = [arr[lastChildIndex], arr[0]];
+        heapify(arr, lastChildIndex, 0);
+        lastChildIndex--;
+    }
+    return arr;
+}
+console.log(heapSort([4,6,3,2]));
